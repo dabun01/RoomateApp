e0f2f9fce7dbe5e482384ce99e257d63408f3f43
dabun01 <dabun01@jaguar.tamu.edu>
2025-04-10 22:43:52 -0500

    Placed user in its own file chanaged profile picture to be a string

--- RoomateApp/Views/ProfileView.swift
+++ RoomateApp/Views/ProfileView.swift
@@ -8,41 +8,33 @@
 import SwiftUI
 
 struct ProfileView: View {
-    let users: [User] = [
-        User(name: "David", profilePicture: UIImage(named: "profilePicture")),
-        User(name: "Chris", profilePicture: nil),
-        User(name: "Ruben", profilePicture: UIImage(named: "profilePicture")),
-        User(name: "Manny", profilePicture: UIImage(named: "profilePicture"))
-    ]
-    
+   
     @State private var notifications = true
-    
-    
-    
+    let currentUser = users[0]
     var body: some View {
         VStack{
             VStack {
                 Image("profilePicture")
                     .resizable()
                     .frame(width: 300, height: 300)
-                Text("\(users[0].name)")
+                Text("\(currentUser.name)")
                     .font(.largeTitle)
             }
-
-            NavigationStack{
-                Form{
-                    Text("Settings")
-                    Text("Username: \(users[0].name)")
-                    Text("Notifications")
-                    Toggle(isOn: $notifications) {
-                        Text("Notifications")
+           
+                NavigationStack{
+                    Form{
+                        Text("Username: \(currentUser.name)")
+                        Toggle(isOn: $notifications) {
+                            Text("Notifications")
+                        }
+                        Section("Users"){
+                            ForEach(users, id: \.self) { user in
+                                Text(user.name)
+                            }
+                        }
                     }
-                    
+                    .navigationTitle("Settings")
                 }
-            }
-            .navigationTitle("User Options")
-
-
         }
         .padding()
     }
