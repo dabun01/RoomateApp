2606c87c752a6d9277b49973ceda69a2aec510c6
dabun01 <dabun01@jaguar.tamu.edu>
2025-04-14 22:40:31 -0500

    Updated ProfileView add nickname and background color

--- RoomateApp/Views/ProfileView.swift
+++ RoomateApp/Views/ProfileView.swift
@@ -8,35 +8,80 @@
 import SwiftUI
 
 struct ProfileView: View {
-   
+    let backgroundGradient = LinearGradient(
+        colors: [Color.green, Color.purple],
+        startPoint: .top, endPoint: .bottom)
+    
     @State private var notifications = true
-    let currentUser = users[0]
+    @State private var currentUser = users[0]
     var body: some View {
-        VStack{
+        ZStack{
+            backgroundGradient
             VStack {
-                Image("profilePicture")
-                    .resizable()
-                    .frame(width: 300, height: 300)
-                Text("\(currentUser.name)")
-                    .font(.largeTitle)
-            }
-           
+                HStack{
+                    VStack{
+                        Image("profilePicture")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(.circle)
+                            .frame(width: 125, height: 125)
+                            .padding()
+                        Text("\(currentUser.name)")
+                            .font(.largeTitle)
+                            .foregroundStyle(Color.white)
+                    }
+                    Rectangle()
+                            .fill(Color.white)
+                            .frame(width: 10)
+                            .padding(.vertical, 10)
+                            .overlay(
+                                Rectangle()
+                                    .stroke(style: StrokeStyle(lineWidth: 1, dash: [1]))
+                                    .foregroundColor(.white)
+                            )
+                    
+                    VStack{
+                        Text("\(currentUser.points)")
+                        Text("PTS")
+                    }
+                    .font(.title)
+                    .foregroundStyle(Color.white)
+                    .fontDesign(.rounded)
+                    .padding()
+                }
                 NavigationStack{
                     Form{
-                        Text("Username: \(currentUser.name)")
-                        Toggle(isOn: $notifications) {
-                            Text("Notifications")
+                        Section("Nickname (Tap to edit)"){
+                            TextField("\(currentUser.name)", text: $currentUser.name)
+                                .foregroundStyle(Color.black)
                         }
+                        Section("Notifications"){
+                            Toggle(isOn: $notifications) {
+                                Text("Chore Alerts")
+                            }
+                        }
+                        
                         Section("Users"){
                             ForEach(users, id: \.self) { user in
                                 Text(user.name)
                             }
                         }
+                        Text("Logout")
+                            .foregroundStyle(Color.red)
+                        Section("Version"){
+                            Text("1.0")
+                        }
                     }
-                    .navigationTitle("Settings")
+                    .navigationTitle("Settings").navigationBarTitleDisplayMode(.automatic)
                 }
+                .frame(width: 350, height: 510)
+                .cornerRadius(20)
+                
+                
+            }
+            .padding()
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
